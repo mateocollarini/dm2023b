6fc9cd1b6c2aa4dd4e56ac04b9cb6c50bff93741
mateocollarini <mrcollarini@gmail.com>
2023-09-22 21:20:13 +0000

    auto commit 1       20230922 212012

--- src/rpart/z222_traintest_montecarlo.r
+++ src/rpart/z222_traintest_montecarlo.r
@@ -7,7 +7,7 @@ require("parallel")
 
 PARAM <- list()
 # reemplazar por las propias semillas
-PARAM$semillas <- c(102191, 200177, 410551, 552581, 892237)
+PARAM$semillas <- c(4000379, 5000623, 5001413, 8001491, 8001899)
 
 #------------------------------------------------------------------------------
 # particionar agrega una columna llamada fold a un dataset que consiste
@@ -78,7 +78,7 @@ ArbolEstimarGanancia <- function(semilla, param_basicos) {
 #------------------------------------------------------------------------------
 
 # Aqui se debe poner la carpeta de la computadora local
-setwd("X:\\gdrive\\ITBA2023B\\") # Establezco el Working Directory
+setwd("~/buckets/b1") # Establezco el Working Directory
 # cargo los datos
 
 # cargo los datos
